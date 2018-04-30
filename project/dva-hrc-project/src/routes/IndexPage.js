1d34a709993444d418e8df1c519f51dce92bb93a
LBC <847129673@qq.com>
2018-04-30 17:17:57 +0800

    2018-4-30-0.1

--- project/dva-hrc-project/src/routes/IndexPage.js
+++ project/dva-hrc-project/src/routes/IndexPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'dva';
-// import { Button } from 'react-bootstrap';
 import styles from './IndexPage.css';
 
 function IndexPage() {
@@ -9,7 +8,6 @@ function IndexPage() {
       <h1 className={styles.title}>Yay! Welcome to dva!</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
-      {/* <Button>234</Button> */}
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
         <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
       </ul>
