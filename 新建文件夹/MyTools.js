2ca7eda8a7f03df800a78d40c5442249ba260e78
LBC <847129673@qq.com>
2018-07-06 20:10:12 +0800

    2018-7-6-1

--- 新建文件夹/MyTools.js
+++ 新建文件夹/MyTools.js
@@ -1,17 +1,6 @@
-[].slice.call(类数组)
-document.getElementById('')
-document.getElementsByClassName('')[0].getElementsByTagName
-array.forEach(element => {
-    
-});
-for (var j = 0; j < array.length; j++) {
-    
-}
-
-
-!function(){
-    //选项卡封装。参数一：类数组或数组。参数二：默认选中。参数三：类名。默认active
-    function tabFun(element, activeNum, className="active") {
+var MyTools = {
+    tabFun: function (element, activeNum, className="active") {
+        //选项卡封装。参数一：类数组或数组。参数二：默认选中。参数三：类名。默认active
         var arr = [].slice.call(element);
         for (var j = 0; j < arr.length; j++) {
             arr[j].classList.remove(className);
@@ -26,4 +15,4 @@ for (var j = 0; j < array.length; j++) {
             }
         })
     }
-}();
+};
