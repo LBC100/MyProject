23f122e9f3c57f1e8717a371471f1b72d657af0c
LBC <847129673@qq.com>
2018-08-07 11:19:02 +0800

    2018-8-7-2

--- 新建文件夹/1.js
+++ 新建文件夹/1.js
@@ -27,3 +27,37 @@ for (var j = 0; j < array.length; j++) {
         })
     }
 }();
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+import React from 'react';
+import Switch from 'react-router/Switch';
+import Route from 'react-router/Route';
+
+var renderRoutes = function renderRoutes(routes) {
+  var extraProps = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  return routes ? React.createElement(
+    Switch,
+    null,
+    routes.map(function (route, i) {
+      return React.createElement(Route, {
+        key: route.key || i,
+        path: route.path,
+        exact: route.exact,
+        strict: route.strict,
+        render: function render(props) {
+          return React.createElement(route.component, _extends({}, props, extraProps, { route: route }));
+        }
+      });
+    })
+  ) : null;
+};
+
+export default renderRoutes;
+
+
+//////////////////
+// WEBPACK FOOTER
+// ./node_modules/react-router-config/es/renderRoutes.js
+// module id = 1225
+// module chunks = 9
