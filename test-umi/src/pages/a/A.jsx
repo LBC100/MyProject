16153d34c88c3816d8970b51f810473810efe672
wu <1883828@qq.com>
2019-04-17 11:36:22 +0800

    2019-4-17-1

---
+++ test-umi/src/pages/a/A.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class A extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() {
+    return (
+      <div>
+        页面a
+      </div>
+    );
+  }
+}
+ 
+export default A;
